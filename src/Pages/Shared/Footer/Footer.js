a693bc102db7a46a7901ac9bf830802a38d69c8c
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-11-29 03:10:47 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,33 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import logo from '../../../images/logo.png';
 const Footer = () => {
     return (
         <div>
-            <h2>Footer</h2>
+            <footer className="footer p-10 bg-base-200 text-base-content">
+                <div>
+                   <Link to='/'>
+                   <img src={logo} alt='book' className='w-20'/>
+                    <p className="link link-hover font-lobster text-xl">Share Knowledge</p>
+                   </Link>
+                </div>
+                <div>
+                    <span className="footer-title">Services</span>
+                    <Link className="link link-hover">Book Selling</Link>
+                    <Link className="link link-hover">Advertisement</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <Link className="link link-hover">About us</Link>
+                    <Link className="link link-hover">Contact</Link>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <Link className="link link-hover">Terms of use</Link>
+                    <Link className="link link-hover">Privacy policy</Link>
+                    <Link className="link link-hover">Cookie policy</Link>
+                </div>
+            </footer>
         </div>
     );
 };
